0c9b62c71732d9c50c6748578fb5b9400021d25e
Christopher <christophermelendez13@gmail.com>
2021-04-24 18:50:17 -0600

    App de contador

---
+++ contador/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Nosotros escribimos
+// import 'src/app.dart';
+import 'package:contador/src/app.dart';
+
+void main() {
+  runApp( MyApp() );
+}
---
+++ contador/lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:contador/src/pages/contador_page.dart';
+// import 'package:contador/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build( context ) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        // child: HomePage(),
+        child: ContadorPage(),
+      ),
+    );
+  }
+}
