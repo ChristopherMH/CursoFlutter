c70899a49513770079745155ba4816244828150c
Christopher <christophermelendez13@gmail.com>
2021-07-12 22:29:40 -0600

    Bloc de Productos añadido

--- flutter-form-validation-1.0.0/lib/src/pages/home_page.dart
+++ flutter-form-validation-1.0.0/lib/src/pages/home_page.dart
@@ -6,38 +6,35 @@ import 'package:formvalidation/src/providers/productos_provider.dart';
 
 class HomePage extends StatelessWidget {
   
-  final productosProvider = new ProductosProvider();
+  // final productosProvider = new ProductosProvider();
   
   @override
   Widget build(BuildContext context) {
 
-    final bloc = Provider.of(context);
+    // final bloc = Provider.of(context);
     
+    final productosBloc = Provider.productosBloc(context);
+    productosBloc.cargarProducts();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Home')
       ),
-      body: _crearListado(),
+      body: _crearListado(productosBloc),
       floatingActionButton: _crearBoton( context ),
     );
   }
 
-
-  Widget _crearListado() {
-
-    return FutureBuilder(
-      future: productosProvider.cargarProductos(),
+  Widget _crearListado(ProductosBloc productosBloc) {
+    return StreamBuilder(
+      stream: productosBloc.productosStream,
       builder: (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
         if ( snapshot.hasData ) {
-
           final productos = snapshot.data;
-
           return ListView.builder(
             itemCount: productos.length,
-            itemBuilder: (context, i) => _crearItem(context, productos[i] ),
+            itemBuilder: (context, i) => _crearItem(context, productosBloc, productos[i] ),
           );
-
         } else {
           return Center( child: CircularProgressIndicator());
         }
@@ -45,7 +42,7 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _crearItem(BuildContext context, ProductoModel producto ) {
+  Widget _crearItem(BuildContext context, ProductosBloc productosBloc,ProductoModel producto ) {
 
     return Dismissible(
       key: UniqueKey(),
@@ -53,7 +50,8 @@ class HomePage extends StatelessWidget {
         color: Colors.red,
       ),
       onDismissed: ( direccion ){
-        productosProvider.borrarProducto(producto.id);
+        // productosProvider.borrarProducto(producto.id);
+        productosBloc.borrarProducto(producto.id);
       },
       child: Card(
         child: Column(
